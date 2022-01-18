bf72a24a09c513f843110d906b45e2bfe1b2bc3f
KirillGul <50946729+KirillGul@users.noreply.github.com>
2022-01-18 22:27:27 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мои скрипты</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/upload.svg" class="card-img-top" alt="Upload Image">
+                    <div class="card-body">
+                      <h5 class="card-title"Upload</h5>
+                      <p class="card-text">Скрипт для загрузки файлов на сервер (папку).</p>
+                      <a href="upload/index.php" class="btn btn-primary">Перейти</a>
+                    </div>
+                  </div>
+            </div>
+          </div>       
+      </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
